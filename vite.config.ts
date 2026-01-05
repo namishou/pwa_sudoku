cf7ba98e131fe2e4963ecc56018165b2e0018590
namishou <namishou@gmail.com>
2026-01-06 00:14:40 +0900

    Add the base URL.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://namishou.github.io/pwa_sudoku/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
